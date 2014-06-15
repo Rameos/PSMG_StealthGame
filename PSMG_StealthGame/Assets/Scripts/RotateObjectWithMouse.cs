d6a011bf136a49aa801eb8219da4e30d4229ff08
f-reichel <3razor16@gmail.com>
2014-06-16 01:01:01 +0200

    object can now be rotated horizontally RotateObjectWithMouse.cs closes #17

---
+++ PSMG_StealthGame/Assets/Scripts/RotateObjectWithMouse.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObjectWithMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 30 * Input.GetAxis("Mouse X"), 0);	
+	}
+}
