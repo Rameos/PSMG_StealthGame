443a852a671c9b36b1fdb3e1c92a79004f1cc68c
Qoes <Qoes@hotmail.de>
2014-07-11 11:13:08 +0200

    Sounds fast abspielbar
    datenstuktur steht schon, aber sound werden nicht abgespielt

---
+++ PSMG_StealthGame/Assets/Scripts/AlphaLevelScripts/Behaviour/Barkeeper/greenSpot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public class greenSpot : MonoBehaviourWithGazeComponent{
+	GameObject Kellner;
+	float waitForIt = 3f;
+	bool stillLooking = false;
+	
+	void Start () {
+		Kellner = GameObject.FindGameObjectWithTag("waiter");
+	}
+	
+	void Update () {
+		
+	}
+	
+	public override void OnGazeEnter(RaycastHit hit)
+	{
+		stillLooking = true;
+		Kellner.GetComponent<dealWithPlayer>().dealWithMessage("GreenSpot");
+		
+		StartCoroutine(callLater(waitForIt));
+	}
+	
+	public override void OnGazeExit()
+	{
+		stillLooking = false;
+	}
+	
+	public override void OnGazeStay(RaycastHit hit)
+	{
+		
+	}
+	
+	IEnumerator callLater(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		if (stillLooking) 
+		{
+			//Kellner.GetComponent<KellnerTalk>().dealWithMessage("EyeLong");
+		}
+		
+	}
+}
