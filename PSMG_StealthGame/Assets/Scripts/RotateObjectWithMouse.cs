13f46c72daa9bc9e4fcf3810d78b37f30aa3884b
f-reichel <3razor16@gmail.com>
2014-07-11 23:15:17 +0200

    rotate with mouse script now checks for press of left mouse button before rotating object is executed

--- PSMG_StealthGame/Assets/Scripts/RotateObjectWithMouse.cs
+++ PSMG_StealthGame/Assets/Scripts/RotateObjectWithMouse.cs
@@ -10,25 +10,28 @@ public class RotateObjectWithMouse : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetAxis("Mouse X") > 0)
+        if (Input.GetMouseButton(0))
         {
-            Debug.Log("Right");
-            transform.Rotate(0, 3, 0, Space.World);
-        }
-        else if (Input.GetAxis("Mouse X") < 0)
-        {
-            Debug.Log("Left");
-            transform.Rotate(0, -3, 0, Space.World);
-        }
-        else if (Input.GetAxis("Mouse Y") > 0)
-        {
-            Debug.Log("Up");
-            transform.Rotate(3, 0, 0, Space.World);
-        }
-        else if (Input.GetAxis("Mouse Y") < 0)
-        {
-            Debug.Log("Down");
-            transform.Rotate(-3, 0, 0, Space.World);
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                Debug.Log("Right");
+                transform.Rotate(0, 3, 0, Space.World);
+            }
+            else if (Input.GetAxis("Mouse X") < 0)
+            {
+                Debug.Log("Left");
+                transform.Rotate(0, -3, 0, Space.World);
+            }
+            else if (Input.GetAxis("Mouse Y") > 0)
+            {
+                Debug.Log("Up");
+                transform.Rotate(3, 0, 0, Space.World);
+            }
+            else if (Input.GetAxis("Mouse Y") < 0)
+            {
+                Debug.Log("Down");
+                transform.Rotate(-3, 0, 0, Space.World);
+            }
         }
 	}
 }
