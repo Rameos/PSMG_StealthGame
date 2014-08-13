35c55752197aa80c366709b6090f930198f0ed76
f-reichel <3razor16@gmail.com>
2014-08-13 04:25:38 +0200

    wip

---
+++ ReallySeriousGame v0.1/Assets/_Scripts/Suspects/DialogManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogManager : MonoBehaviour {
+
+    public delegate void DialogEvent(string Data);
+    public static event DialogEvent OnTalking;
+    public static event DialogEvent OnLeaving;
+    private bool active = true;
+    private Suspect suspect;
+
+	// Use this for initialization
+	void Awake () {
+        suspect = GameObject.FindGameObjectWithTag("Suspect").GetComponent<Suspect>();
+        Debug.Log("Number of Conversations: " + suspect.numberOfConversations);
+        if (OnTalking != null && active == true)
+        {
+            OnTalking("Gruener Fleck");
+            active = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OnLeaving != null && active == false)
+        {
+            suspect.numberOfConversations++;
+            OnLeaving("Bye");
+        }
+
+	}
+}
