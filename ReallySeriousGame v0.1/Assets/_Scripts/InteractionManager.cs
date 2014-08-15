ea3375cb9b2d1dd37e032e38808353c4d3b4ba07
yifan9916 <yifan.9916@gmail.com>
2014-08-15 18:37:54 +0200

    Bug fixes and refactored InputController/PauseMenu

--- ReallySeriousGame v0.1/Assets/_Scripts/InteractionManager.cs
+++ ReallySeriousGame v0.1/Assets/_Scripts/InteractionManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class InteractionManager : MonoBehaviour 
 {
+	bool isInteracting = false;
+
 	Vector3 itemOriginalPos;
 	public float itemDistanceFromCamera = 2f;
 	GameObject currentItem;
@@ -12,21 +14,89 @@ public class InteractionManager : MonoBehaviour
 	GameObject currentSuspect;
 	
 	/// <summary>
+	/// Starts interaction with selected object.
+	/// </summary>
+	public void StartInteraction(GameObject selection)
+	{
+		if(!isInteracting)
+		{
+			switch(selection.tag)
+			{
+			case "Interactable": 
+				GameState.ChangeState(GameState.States.Inspecting);
+				Inspect(selection); 
+				break;
+				
+			case "Suspect":
+				GameState.ChangeState(GameState.States.Interrogating);
+				Interrogate(selection); 
+				break;
+				
+			case "Door":
+				GameState.ChangeState(GameState.States.InGame);
+				EnterDoor(); 
+				break;
+				
+			default: break;
+			}
+			isInteracting = true;
+		}
+		else
+		{
+			return;
+		}
+	}
+	
+	/// <summary>
+	/// Stops interaction with selected object.
+	/// </summary>
+	public void StopInteraction()
+	{
+		if(isInteracting)
+		{
+			switch(GameState.gameState)
+			{
+			case GameState.States.Inspecting:
+				GameState.ChangeState(GameState.States.InGame);
+				StopInspection();
+				break;
+				
+			case GameState.States.Interrogating:
+				GameState.ChangeState(GameState.States.InGame);
+				StopInterrogation();
+				break;
+				
+			default: break;
+			}
+			isInteracting = false;
+		}
+		else
+		{
+			return;
+		}
+	}
+	
+	public bool IsInteracting()
+	{
+		return isInteracting;
+	}
+	
+	/// <summary>
 	/// Pull object into camera center.
 	/// </summary>
-	
 	public void Inspect(GameObject item)
 	{
 		currentItem = item;
+		#region position item
 		itemOriginalPos = currentItem.transform.position;
 		transform.LookAt(currentItem.transform);
 		currentItem.transform.position = Camera.main.transform.position + Camera.main.transform.forward * itemDistanceFromCamera;
+		#endregion
 	}
 	
 	/// <summary>
 	/// Quit Inspection and return item to its original position
 	/// </summary>
-	
 	public void StopInspection()
 	{
 		currentItem.transform.position = itemOriginalPos;
@@ -35,20 +105,20 @@ public class InteractionManager : MonoBehaviour
 	/// <summary>
 	/// Move Player towards suspect.
 	/// </summary>
-	
 	public void Interrogate(GameObject suspect)
 	{
 		currentSuspect = suspect;
+		#region position player
 		playerOriginalPos = transform.position;
 		Vector3 suspectPos = new Vector3(currentSuspect.transform.position.x, transform.position.y, currentSuspect.transform.position.z); //Lock Y-Axis
 		transform.LookAt(suspectPos);
 		transform.position = Vector3.Lerp(transform.position, currentSuspect.transform.position, suspectDistanceFromPlayer);
+		#endregion
 	}
 	
 	/// <summary>
 	/// Quit Interrogation and return player to his original location
 	/// </summary>
-	
 	public void StopInterrogation()
 	{
 		transform.position = playerOriginalPos;
@@ -57,7 +127,6 @@ public class InteractionManager : MonoBehaviour
 	/// <summary>
 	/// Transition to next level
 	/// </summary>
-	
 	public void EnterDoor()
 	{
 		Application.LoadLevel("Scene_2");
@@ -65,12 +134,14 @@ public class InteractionManager : MonoBehaviour
 	}
 	
 	public void RotateItemLeft(GameObject interactable)
-	{
-		interactable.transform.Rotate(Vector3.up * 50 * Time.deltaTime,Space.World);
+	{	
+		if(GameState.IsState(GameState.States.Inspecting))
+			interactable.transform.Rotate(Vector3.up * 50 * Time.deltaTime,Space.World);
 	}
 	
 	public void RotateItemRight(GameObject interactable)
 	{
-		interactable.transform.Rotate(Vector3.down * 50 * Time.deltaTime,Space.World);
+		if(GameState.IsState(GameState.States.Inspecting))
+			interactable.transform.Rotate(Vector3.down * 50 * Time.deltaTime,Space.World);
 	}
 }
