5cc83ba90e8d90ff8bb753d98f4592b802092141
Qoes <Qoes@hotmail.de>
2014-08-27 05:43:48 +0200

    notebook almost finished
    code überarbeitet

--- ReallySeriousGame v0.1/Assets/_Scripts/Clues/ClueManager.cs
+++ ReallySeriousGame v0.1/Assets/_Scripts/Clues/ClueManager.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 
 public class ClueManager : MonoBehaviour 
 {
-	public static ClueManager clueManager;
-	public Texture drink, mixbuch, gift, pille, pflanze, docbuch;
-
+	//old
 	static List<string> foundClues = new List<string>();
-	static List<bool> noteBookClues = new List<bool>();
 
-	// verbugt
-	static List<Texture> clueNotes;
+
+	public static ClueManager clueManager;
+
+	public Texture bmDrink, bmMixbuch, bmGift, bmHand, bmFleck;
+	public Texture docPille, docPlfanze, docBuch;
+	public Texture detTatwaffe, detStoff;
+	public Texture bmDrinkNote, bmMixbuchNote, bmGiftNote, bmHandNote, bmFleckNote, docPilleNote, docPlfanzeNote, docBuchNote, detStoffNote, detTatwaffeNote;
+
+	static List<string> foundCluesBarmann = new List<string>();
+	static List<string> foundCluesDoctor = new List<string>();
+	static List<string> foundCluesDetective = new List<string>();
+
 	
-
+	private Dictionary<string, Texture> nameToTexture = new Dictionary<string, Texture>();
+	private Dictionary<string, Texture> nameToTextureNote = new Dictionary<string, Texture>();
+	
 	Clue selectedClue;
 	
 	void Awake ()
@@ -30,18 +39,49 @@ public class ClueManager : MonoBehaviour
 		}
 		#endregion
 
-		for (int i = 0; i < 6; i++) {
-			noteBookClues.Add(false);
-		}
+		fillLists ();
 
-		// verbugt
-		List<Texture> clueNotes = new List<Texture>()
-		{
-			drink, mixbuch, gift, pille, pflanze, docbuch
-		};
-		Debug.Log ("nitch " +clueNotes[0]);
+		fillDetectiveNotes ();
+
 	}
-	
+
+	private void fillDetectiveNotes () {
+		foundCluesDetective.Add ("detTatwaffe");
+		foundCluesDetective.Add ("detStoff");
+	}
+
+	private void fillLists (){
+		fillClueDict();
+		fillClueNoteDict();
+	}
+
+	private void fillClueDict () {
+		nameToTexture.Add("barmannDrink", bmDrink);
+		nameToTexture.Add("barmannMixBuch", bmMixbuch);
+		nameToTexture.Add("barmannGift", bmGift);
+		nameToTexture.Add("barmannHand", bmHand);
+		nameToTexture.Add("barmannFleck", bmFleck);
+		nameToTexture.Add("docPille", docPille);
+		nameToTexture.Add("docPflanze", docPlfanze);
+		nameToTexture.Add("docBuch", docBuch);
+		nameToTexture.Add("detTatwaffe", detTatwaffe);
+		nameToTexture.Add("detStoff", detStoff);
+
+	}
+
+	private void fillClueNoteDict () {
+		nameToTextureNote.Add("barmannDrink", bmDrinkNote);
+		nameToTextureNote.Add("barmannMixBuch", bmMixbuchNote);
+		nameToTextureNote.Add("barmannGift", bmGiftNote);
+		nameToTextureNote.Add("barmannHand", bmHandNote);
+		nameToTextureNote.Add("barmannFleck", bmFleckNote);
+		nameToTextureNote.Add("docPille", docPilleNote);
+		nameToTextureNote.Add("docPflanze", docPlfanzeNote);
+		nameToTextureNote.Add("docBuch", docBuchNote);
+		nameToTextureNote.Add("detTatwaffe", detTatwaffeNote);
+		nameToTextureNote.Add("detStoff", detStoffNote);
+	}
+
 	/// <summary>
 	/// Activate all Clue objects on selected GameObject.
 	/// </summary>
@@ -80,24 +120,24 @@ public class ClueManager : MonoBehaviour
 	{
 		selectedClue = newClue.GetComponent<Clue>();
 		selectedClue.SetDiscovered();
-
-		Debug.Log (selectedClue.clueName);
-		if (selectedClue.clueName.Contains("Drink")) {
-			noteBookClues[0] = true;
-		}if (selectedClue.clueName.Contains("Mixbuch")) {
-			noteBookClues[1] = true;
-		}if (selectedClue.clueName.Contains("Gift")) {
-			noteBookClues[2] = true;
-		}if (selectedClue.clueName.Contains("Pille")) {
-			noteBookClues[3] = true;
-		}if (selectedClue.clueName.Contains("Pflanze")) {
-			noteBookClues[4] = true;
-		}if (selectedClue.clueName.Contains("Docbuch")) {
-			noteBookClues[5] = true;
+		if (isAlreadyFound(selectedClue.clueName)) {
+			if (selectedClue.clueName.Substring(0, 3) == "bar") {
+				foundCluesBarmann.Add(selectedClue.clueName);
+			}
+			if (selectedClue.clueName.Substring(0, 3) == "doc") {
+				foundCluesDoctor.Add(selectedClue.clueName);
+			}
+			foundClues.Add(selectedClue.clueName);
 		}
+	}
 
-		// old
-		//foundClues.Add(selectedClue.clueName);
+	private bool isAlreadyFound (string clueName) {
+		for (int i = 0; i < foundClues.Count; i++){
+			if (foundClues[i] == clueName) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	/// <summary>
@@ -109,13 +149,28 @@ public class ClueManager : MonoBehaviour
 		return foundClues;
 	}
 
-	// verbugt
-	public Texture GetClueNotesAtPosition(int index)
+	public List<string> GetCluesBarmann() 
 	{
-		return clueNotes[index];
+		return foundCluesBarmann;
 	}
 
-	public bool CheckClue (int cluePosition) {
-		return noteBookClues [cluePosition];
+	public List<string> GetCluesDoctor() 
+	{
+		return foundCluesDoctor;
+	}
+
+	public List<string> GetCluesDetective() 
+	{
+		return foundCluesDetective;
+	}
+
+	public Texture GetCluesTexture(string name) 
+	{
+		return nameToTexture[name];
+	}
+	
+	public Texture GetCluesNoteTexture(string name) 
+	{
+		return nameToTextureNote[name];
 	}
 }
--- ReallySeriousGame v0.1/Assets/_Scripts/Clues/NoteBook.cs
+++ ReallySeriousGame v0.1/Assets/_Scripts/Clues/NoteBook.cs
@@ -9,10 +9,6 @@ public class NoteBook : MonoBehaviour
 	public GUIStyle notebookStyle;
 	public GUIStyle btnStyle;
 	public Texture notebookTexture;
-	public Texture barmannClue1, barmannClue2, barmannClue3;
-	public Texture docClue1, docClue2, docClue3;
-	//übergangsweise, muss noch refactored werden
-	public Texture drink, mixbuch, gift, pille, pflanze, docbuch;
 	private Texture clueNote;
 
 	private float defaultButtonSize;
@@ -26,10 +22,18 @@ public class NoteBook : MonoBehaviour
 	private float offset			= 30;
 	
 	private Rect note;
-	private float noteHeight;
-	private float noteWidth;
+//	private float noteHeight;
+//	private float noteWidth;
 	
-	private List<string> notes;
+//	private List<string> notes;
+	private List<string> cluesBarmann;
+	private List<string> cluesDoctor;
+	private List<string> cluesDetective;
+
+	private string barmannsCluesName = "barmannClues";
+	private string doctorCluesName = "doctorClues";
+	private string detectiveCluesName = "detectiveClues";
+
 
 	private const int NOTEBOOK_ID = 0;
 	private string notebookHeader = "NOTES";
@@ -44,8 +48,8 @@ public class NoteBook : MonoBehaviour
 		notebookY 	= Screen.height - notebookHeight;
 		notebook 	= new Rect(notebookX, notebookY, notebookWidth, notebookHeight);
 		
-		noteHeight 	= notebookHeight * 0.1f;
-		noteWidth 	= notebookWidth - (offset * 2);
+//		noteHeight 	= notebookHeight * 0.1f;
+//		noteWidth 	= notebookWidth - (offset * 2);
 	}
 	
 	void OnGUI()
@@ -55,7 +59,10 @@ public class NoteBook : MonoBehaviour
 		if(isToggled)
 		{
 		
-			notes = clue.GetFoundClues();
+//			notes = clue.GetFoundClues();
+			cluesBarmann = clue.GetCluesBarmann();
+			cluesDoctor = clue.GetCluesDoctor();
+			cluesDetective = clue.GetCluesDetective();
 
 			notebook = GUI.Window(NOTEBOOK_ID, notebook, DisplayFoundClues, notebookHeader, notebookStyle);
 		}
@@ -63,74 +70,98 @@ public class NoteBook : MonoBehaviour
 	
 	void DisplayFoundClues(int ID)
 	{
-		defaultButtonSize =notebook.width * 0.18f;
-
-
+		defaultButtonSize = notebook.width * 0.18f;
 
 		GUI.DrawTexture(new Rect(0,0,notebook.width,notebook.height),notebookTexture,ScaleMode.StretchToFill,true);
 
-
-
-
-		if (clue.CheckClue(0)) {
-			if (GUI.Button(new Rect(notebook.width*0.17f, notebook.height*0.23f, defaultButtonSize, defaultButtonSize), barmannClue1, btnStyle)) {
-				clueNote = drink;
-				clueNotesActive = true;
-			}
-		}
-		if (clue.CheckClue(1)) {
-			if (GUI.Button(new Rect(notebook.width*0.41f, notebook.height*0.23f, defaultButtonSize, defaultButtonSize), barmannClue2, btnStyle)) {
-				clueNote = mixbuch;
-				clueNotesActive = true;
-			}
-		}
-		if (clue.CheckClue(2)) {
-			if (GUI.Button(new Rect(notebook.width*0.65f, notebook.height*0.23f, defaultButtonSize, defaultButtonSize), barmannClue3, btnStyle)) {
-				clueNote = gift;
-				clueNotesActive = true;
-			}
-		}
-		if (clue.CheckClue(3)) {
-			if (GUI.Button(new Rect(notebook.width*0.17f, notebook.height*0.39f, defaultButtonSize, defaultButtonSize), docClue1, btnStyle)) {
-				clueNote = pille;
-				clueNotesActive = true;
-			}
-		}
-		if (clue.CheckClue(4)) {
-			if (GUI.Button(new Rect(notebook.width*0.41f, notebook.height*0.39f, defaultButtonSize, defaultButtonSize), docClue2, btnStyle)) {
-				clueNote = pflanze;
-				clueNotesActive = true;
-			}
-		}
-		if (clue.CheckClue(5)) {
-			if (GUI.Button(new Rect(notebook.width*0.65f, notebook.height*0.39f, defaultButtonSize, defaultButtonSize), docClue3, btnStyle)) {
-				clueNote = docbuch;
-				clueNotesActive = true;
-			}
-		}
+		showClues (cluesBarmann, barmannsCluesName);
+		showClues (cluesDoctor, doctorCluesName);
+		showClues (cluesDetective, detectiveCluesName);
+//
+//
+//		if (clue.CheckClue(0)) {
+//			if (GUI.Button(new Rect(notebook.width*0.17f, notebook.height*0.23f, defaultButtonSize, defaultButtonSize), barmannClue1, btnStyle)) {
+//				clueNote = drink;
+//				clueNotesActive = true;
+//			}
+//		}
+//		if (clue.CheckClue(1)) {
+//			if (GUI.Button(new Rect(notebook.width*0.41f, notebook.height*0.23f, defaultButtonSize, defaultButtonSize), barmannClue2, btnStyle)) {
+//				clueNote = mixbuch;
+//				clueNotesActive = true;
+//			}
+//		}
+//		if (clue.CheckClue(2)) {
+//			if (GUI.Button(new Rect(notebook.width*0.65f, notebook.height*0.23f, defaultButtonSize, defaultButtonSize), barmannClue3, btnStyle)) {
+//				clueNote = gift;
+//				clueNotesActive = true;
+//			}
+//		}
+//		if (clue.CheckClue(3)) {
+//			if (GUI.Button(new Rect(notebook.width*0.17f, notebook.height*0.39f, defaultButtonSize, defaultButtonSize), docClue1, btnStyle)) {
+//				clueNote = pille;
+//				clueNotesActive = true;
+//			}
+//		}
+//		if (clue.CheckClue(4)) {
+//			if (GUI.Button(new Rect(notebook.width*0.41f, notebook.height*0.39f, defaultButtonSize, defaultButtonSize), docClue2, btnStyle)) {
+//				clueNote = pflanze;
+//				clueNotesActive = true;
+//			}
+//		}
+//		if (clue.CheckClue(5)) {
+//			if (GUI.Button(new Rect(notebook.width*0.65f, notebook.height*0.39f, defaultButtonSize, defaultButtonSize), docClue3, btnStyle)) {
+//				clueNote = docbuch;
+//				clueNotesActive = true;
+//			}
+//		}
 
 
 		if (clueNotesActive) {
-
-			// verbugt, ergibt nullpointer
-			//if (GUI.Button(new Rect(0, 0, notebook.width, notebook.height), clue.GetClueNotesAtPosition(0))) {
-
 			if (GUI.Button(new Rect(0, 0, notebook.width, notebook.height), clueNote)) {
 					clueNotesActive = false;
 			}
 		}
 
-		//old
-		for(int i = 0; i < notes.Count; i++)
-		{
-			if(GUI.Button(new Rect(offset, (offset * 2) + (noteHeight * i), noteWidth, noteHeight), notes[i]))
-				Debug.Log("stuff");
+	}
+	private void showClues (List<string> clues, string whosClues) {
+		float widthMulti = 0.18f;
+		float heightMulti = 0;
+		int multiWidth;
+		if (whosClues.Contains(barmannsCluesName)) {
+			heightMulti = 0.23f;
+		}
+		if (whosClues == doctorCluesName) {
+			heightMulti = 0.50f;
+		}
+		if (whosClues == detectiveCluesName) {
+			heightMulti = 0.76f;
+		}
+
+		for (int i = 0; i < clues.Count; i++) {
+			float buttonPosY;
+			float buttonPosX;
+			
+			if (i < 3) {
+				multiWidth = i;
+				buttonPosY = heightMulti;
+			}else {
+				multiWidth = i-3;
+				buttonPosY = heightMulti + 0.10f;
+			}
+			buttonPosX = widthMulti + 0.23f*multiWidth;
+			
+			if (GUI.Button(new Rect(notebook.width*buttonPosX, notebook.height*buttonPosY, defaultButtonSize, defaultButtonSize), clue.GetCluesTexture(clues[i]), btnStyle)) {
+				clueNote = clue.GetCluesNoteTexture(clues[i]);
+				clueNotesActive = true;
+			}
 		}
 	}
-	
+
+
 	public void ToggleNotebook()
 	{
 		isToggled = !isToggled;
 	}
-	
+
 }
